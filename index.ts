83ac42f06eea0e54ad94848d8d2e2f4b649db902
snozaki <snozaki@netprotections.co.jp>
2022-08-06 20:18:41 +0900

    mod: exports

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
 export {Attempt} from './lib/attempt';
-export {SimpleRetryPolicy} from './lib/policy/simple-retry-policy';
-export {ExponentialBackOffRetryPolicy}
-  from './lib/policy/exponential-backoff-retry-policy';
+export * from './lib/policy/policies';
+export * from './lib/duration';
